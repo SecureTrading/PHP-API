b1b52cf52971971e4789fc8ecf967245b7e842a8
root <root@stpp31.securetrading.net>
2016-05-11 09:50:08 +0000

    Improved benchmarks.  Benchmark now run as part of the integration tests.

--- benchmarks/ApiBench.php
+++ benchmarks/ApiBench.php
@@ -2,16 +2,13 @@
 
 namespace Securetrading\Stpp\JsonInterface\Benchmarks;
 
-require(realpath(__DIR__ . '/../../../autoload.php'));
+require(realpath(__DIR__ . '/../vendor/autoload.php'));
 require(realpath(__DIR__ . '/../tests/Helper.php'));
 
 use \Securetrading\Stpp\JsonInterface\Tests\Integration\ApiTest;
 
 class ApiBench implements \PhpBench\Benchmark {
-  /**
-   * @iterations 6
-   */
-  public function benchApi() {
+  private function _processOneAuth() {
     $helper = new \Securetrading\Unittest\Helper();
 
     $benchDir = $helper->getTestDir('benchmarks');
@@ -37,13 +34,16 @@ class ApiBench implements \PhpBench\Benchmark {
     );
 
     $request = array(
-      'requesttypedescription' => 'CACHETOKENISE',
+      'requesttypedescription' => 'AUTH',
       'pan' => '4111110000000211',
       'expirymonth' => '11',
       'expiryyear' => '2031',
       'securitycode' => '123',
       'paymenttypedescription' => 'VISA',
+      'mainamount' => '10.00',
+      'currencyiso3a' => 'GBP',
       'sitereference' => $testConfig['siteReference'],
+      'accounttypedescription' => 'ECOM',
     );
 
     $api = $ioc->get('\Securetrading\Stpp\JsonInterface\Api', $config);
@@ -53,5 +53,46 @@ class ApiBench implements \PhpBench\Benchmark {
     if ($responseData['responses'][0]['errorcode'] !== '0') {
       throw new \Exception('Bad errorcode: ' . $actualResponseData['responses'][0]['errorcode']);
     } 
+  }
+
+  /**
+   * @iterations 6
+   */
+  public function benchQuickApi() {
+    $this->_processOneAuth();
+  }
+
+  /**
+   * @iterations 1
+   * @paramProvider params_benchFullApi
+   */
+  public function benchFullApi($iteration) {
+    $maxLengthOfTestInSeconds = (int) $iteration->getParameter('max_length_of_test_in_seconds');
+    $transactionsToProcess = (int) $iteration->getParameter('transactions_to_process');
+
+    $processed = false;
+    $startTime = time();
+    $i = 0;
+
+    while((time() - $startTime) < $maxLengthOfTestInSeconds) {
+      $this->_processOneAuth();
+      if ($i++ === $transactionsToProcess) {
+	$processed = true;
+	break;
+      }
+    }
+
+    if (!$processed) {
+      throw new \Exception(sprintf('Ran out of time (%s seconds) before %s transactions could be processed.  %s were processed.', $maxLengthOfTestInSeconds, $transactionsToProcess, $i));
+    }
+  }
+
+  public function params_benchFullApi() {
+    return array(
+      array(
+	'max_length_of_test_in_seconds' => '60',
+	'transactions_to_process' => '500',
+      ),
+    );
   }
 }
